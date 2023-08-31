407dd3eb0a7fa79ceae3e7bc2279cc6f819d7f53
BrunoArmondBraga <abraga3547bruno@gmail.com>
2023-08-31 01:55:56 -0300

    Implements SkeyBinary Kth and minor fixes

--- PersistentStack.cpp
+++ PersistentStack.cpp
@@ -10,15 +10,15 @@ using namespace std;
 class Node
 {
 private:
-    void addLeaf()
+    void addLeaf_b()
     {
-        this->jump[0] = this->next;
+        this->jump_b[0] = this->next;
         double result = log2(this->depth);
         int floor_log = static_cast<int>(floor(result));
 
         for (int i = 1; i <= floor_log; i++)
         {
-            this->jump[i] = this->jump[i - 1]->jump[i - 1];
+            this->jump_b[i] = this->jump_b[i - 1]->jump_b[i - 1];
         }
     }
 
@@ -26,14 +26,39 @@ public:
     int depth;
     int data;
     Node *next;
-    Node *jump[MAX];
+    Node *jump_b[MAX];
+    Node *jump_sb;
 
     Node(int value, Node *next, int depth)
     {
         this->data = value;
         this->next = next;
         this->depth = depth;
-        this->addLeaf();
+        this->addLeaf_b();
+        this->AddLeaf_sb();
+    }
+
+    void AddLeaf_sb(){
+        Node* u = this;
+        Node* v = u->next;
+        int jump_sb_jump_sb_depth = 0;
+        if(v == nullptr){
+            u->jump_sb = nullptr;
+            return;
+        }
+        if(v->jump_sb == nullptr){
+            u->jump_sb = v;
+            return;
+        }
+        if(v->jump_sb->jump_sb != nullptr){
+            jump_sb_jump_sb_depth = v->jump_sb->jump_sb->depth; 
+        }
+        if(v->jump_sb != nullptr && v->depth - v->jump_sb->depth == v->jump_sb->depth - jump_sb_jump_sb_depth){
+            u->jump_sb = v->jump_sb->jump_sb;
+        }
+        else{
+            u->jump_sb = v;
+        }
     }
 };
 
@@ -41,21 +66,21 @@ class PersistentStack
 {
 private:
     Node *root;
-    int numberOfElements;
+    int number_of_elements;
 
-    int level_ancestor_n(int index){
+    int level_ancestor_linear(int k){
         int i = 0;
         Node* newNode = this->root;
-        while (i != index) {
+        while (i != k) {
             if (newNode == nullptr) {
-                cout << "Não há " << index << " itens na pilha escolhida!" << endl;
+                cout << "Não há " << k+1 << " itens na pilha escolhida!" << endl;
                 return -1;
             }
             newNode = newNode->next;
             i++;
         }
         if (newNode == nullptr) {
-            cout << "Não há " << index << " itens na pilha escolhida!" << endl;
+            cout << "Não há " << k+1 << " itens na pilha escolhida!" << endl;
             return -1;
         }
         return newNode->data;
@@ -63,47 +88,61 @@ private:
 
     int level_ancestor_binary(int k){
         Node *u = this->root;
-        if(k < 1){
+        if(k < 0){
             cout << "index inválido para função kth!" << endl;
         }
-        else if (k > this->numberOfElements)
+        else if (k+1 > this->number_of_elements)
         {
-            cout << "Não há " << k << " itens na pilha escolhida!" << endl;
+            cout << "Não há " << k+1 << " itens na pilha escolhida!" << endl;
             return -1;
         }
-        k = k - 1;
         double result = log2(k);
         int floorLog = static_cast<int>(floor(result));
+        int powVariable = pow(2.0, floorLog);
         for (int i = floorLog; i >= 0; i--)
         {
-            int powVariable = pow(2.0, i);
             if (powVariable <= k)
             {
                 k = k - powVariable;
-                u = u->jump[i];
+                u = u->jump_b[i];
             }
+            powVariable /= 2;
         }
         return u->data;
     }
 
     int level_ancestor_skew_binary(int k){
-        return -1;
+        if (k+1 > this->number_of_elements)
+        {
+            cout << "Não há " << k+1 << " itens na pilha escolhida!" << endl;
+            return -1;
+        }
+        Node *u = this->root;
+        int y = u->depth - k;
+        while(u->depth != y){
+            int jump_sb_depth = 0;
+            if(u->jump_sb != nullptr){
+                jump_sb_depth = u->jump_sb->depth;
+            }
+            if(jump_sb_depth >= y){
+                u = u->jump_sb;
+            }
+            else{
+                u = u->next;
+            }
+        }
+        return u->data;
     }
 
 public:
     PersistentStack()
     {
-        this->numberOfElements = 0;
+        this->number_of_elements = 0;
         root = nullptr;
-    }
-    PersistentStack(Node *parent)
-    {
-        this->numberOfElements = 0;
-        this->root = parent;
     }
     PersistentStack(Node *parent, int size)
     {
-        this->numberOfElements = size;
+        this->number_of_elements = size;
         this->root = parent;
     }
     ~PersistentStack()
@@ -114,13 +153,13 @@ public:
     PersistentStack *push(int data)
     {
 
-        int newDepth = 1;
+        int newDepth = 0;
         if (this->root != nullptr)
         {
             newDepth = this->root->depth + 1;
         }
         Node *newNode = new Node(data, this->root, newDepth);
-        PersistentStack *newStack = new PersistentStack(newNode, this->numberOfElements + 1);
+        PersistentStack *newStack = new PersistentStack(newNode, this->number_of_elements + 1);
         return newStack;
     }
 
@@ -132,18 +171,18 @@ public:
             PersistentStack *placeholder = new PersistentStack();
             return placeholder;
         }
-        PersistentStack *newStack = new PersistentStack(root->next, this->numberOfElements - 1);
+        PersistentStack *newStack = new PersistentStack(root->next, this->number_of_elements - 1);
         return newStack;
     }
 
     int size()
     {
-        return this->numberOfElements;
+        return this->number_of_elements;
     }
 
     int top()
     {
-        if (this->numberOfElements == 0)
+        if (this->number_of_elements == 0)
         {
             cout << "Não é possível fazer um top de uma pilha vazia!" << endl;
             return -1;
@@ -153,14 +192,18 @@ public:
 
     int kth(int k, int mode)
     {
+        if(k < 1){
+            cout << "kth(" << k << ") com k inválido!" << endl;
+            return -1;
+        }
         if(mode == 1){
-            return level_ancestor_n(k);
+            return level_ancestor_linear(k-1);
         }
         else if(mode == 2){
-            return level_ancestor_binary(k);
+            return level_ancestor_binary(k-1);
         }
         else{
-            return level_ancestor_skew_binary(k);
+            return level_ancestor_skew_binary(k-1);
         }
     }
 
@@ -180,17 +223,6 @@ public:
             newNode = newNode->next;
         }
         cout << endl;
-    }
-
-    void print()
-    {
-        cout << "====================" << endl;
-        for (int i = 0; i <= 1; i++)
-        {
-            cout << this->root->data << endl;
-            cout << "o ponteiro de valor i = " << i << " possui o valor " << this->root->jump[i]->data << endl;
-        }
-        cout << "====================" << endl;
     }
 };
 
@@ -229,12 +261,12 @@ int main()
     cout << endl;
 
 
-    cout << "O KTH(-1) da pilha p3 é = " << p3->kth(-1,2) << endl;
-    cout << "O KTH(0) da pilha p3 é = " << p3->kth(0,2) << endl;
-    cout << "O KTH(1) da pilha p3 é = " << p3->kth(1,2) << endl;
-    cout << "O KTH(2) da pilha p3 é = " << p3->kth(2,2) << endl;
-    cout << "O KTH(3) da pilha p3 é = " << p3->kth(3,2) << endl;
-    cout << "O KTH(4) da pilha p3 é = " << p3->kth(4,2) << endl;
+    cout << "O KTH(-1) da pilha p3 é = " << p3->kth(-1,1) << endl;
+    cout << "O KTH(0) da pilha p3 é = " << p3->kth(0,1) << endl;
+    cout << "O KTH(1) da pilha p3 é = " << p3->kth(1,1) << endl;
+    cout << "O KTH(2) da pilha p3 é = " << p3->kth(2,1) << endl;
+    cout << "O KTH(3) da pilha p3 é = " << p3->kth(3,1) << endl;
+    cout << "O KTH(4) da pilha p3 é = " << p3->kth(4,1) << endl;
 
     delete p0;
     delete p1;
